1ea250eeaf57013e793df57e8200efb1f70e31e8
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-12-05 12:58:29 -0800

    finished strings

--- 02-strings/parse_strings.py
+++ 02-strings/parse_strings.py
@@ -10,12 +10,42 @@
 #for i in numbers:
 #	print int(i)
 
-rhyme = "One fish two fish red fish blue fish"
+#rhyme = "One fish two fish red fish blue fish"
 
-my_list = []
+#my_list = []
 
-for word in rhyme.split("fish"):
-	if word:
-		my_list.append(word.strip())
+#for word in rhyme.split("fish"):
+#	if word:
+#		my_list.append(word.strip())
 
-print my_list
+#print my_list
+
+#str = "item:apples, quantity:4, price:1.50\n"
+#def grocery_bill(str_input):
+#	str_split = str_input.split(",")
+#	quantity = float(str_split[1].split(":")[1])
+#	price = float(str_split[2].split(":")[1])
+#	return quantity * price
+#print grocery_bill(str)
+
+items = ["item:apples,quantity:4,price:1.50\n", "item:pears,quantity:5,price:2.00\n", "item:cereal,quantity:1,price:4.49\n"]
+
+def grocery_bill(str_input):
+
+	i = 0
+	items_total = 0
+
+	while(i<len(items)):
+		str_split = items[i].split(",")
+		quantity = float(str_split[1].split(":")[1])
+		price = float(str_split[2].split(":")[1])
+		x = quantity*price
+		items_total += x
+		i += 1
+
+	return items_total
+
+print grocery_bill(items)
+
+
+
