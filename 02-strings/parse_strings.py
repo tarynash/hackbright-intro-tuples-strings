e5ca9a309098ca23d788fbbabf89fd2eaf6b4f3b
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-12-05 11:52:34 -0800

    parse

--- 02-strings/parse_strings.py
+++ 02-strings/parse_strings.py
@@ -1,6 +1,21 @@
-my_name = "Taryn"
-name_list = list(my_name)
-print name_list
+#my_name = "Taryn"
+#name_list = list(my_name)
+#print name_list
 
 #numbers = "1,2,3,4,5"
 #print numbers.split(",")
+
+#i = numbers.split(",")
+
+#for i in numbers:
+#	print int(i)
+
+rhyme = "One fish two fish red fish blue fish"
+
+my_list = []
+
+for word in rhyme.split("fish"):
+	if word:
+		my_list.append(word.strip())
+
+print my_list
