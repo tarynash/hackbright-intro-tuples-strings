4fa4987fbeaadd24492aac9ce82ef560b583a56e
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-12-06 15:12:55 -0800

    lists and tuples midway

--- midway.py
+++ midway.py
@@ -29,3 +29,22 @@
 
 #for x in range(1000,0,-100):
 #	print x
+
+##Lists & Tuples
+
+#primary = ("red", "yellow", "blue")
+
+#print primary[2]
+#print primary[-1]
+#print primary[len(primary)-1]
+
+#secondary_colors = ("orange","green","purple")
+#all_colors = primary + secondary_colors
+
+#full_name = ("Taryn", "Shuler")
+
+#full_name = list(full_name)
+
+#full_name = full_name[1], full_name[0]
+
+#print full_name
