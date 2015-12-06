fd5b5002ad9b1b4261dbb75b9b2b44e760298eb9
Taryn Shuler <tarynashleyshuler@gmail.com>
2015-12-06 14:55:38 -0800

    conditionals midway

---
+++ midway.py
@@ -0,0 +1,23 @@
+##Conditionals
+
+#number = 50
+
+#if(number>50):
+#	print "Too high!"
+#elif(number<50):
+#	print "Too low!"
+#else:
+#	print "Just right!"
+
+#object_variable = 3.4567
+
+#if(object_variable != str()):
+#	object_variable = str(object_variable)
+#	print object_variable
+#else:
+#	print object_variable
+
+#print type(object_variable)
+
+##Loops
+
